d1879ce03a40e08bd7fa83223fcf67d98867d457
Juris <juris.abolins.cs@gmail.com>
2021-12-16 19:39:30 +0200

    Final 7.5.1

--- src/juris/Main.java
+++ src/juris/Main.java
@@ -9,18 +9,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<UsesFuel> allFuleUsers = new ArrayList<>();
+        ArrayList<UsesFuel> allFuelUsers = new ArrayList<>();
         Bus bus1 = new Bus("Mercedes", "aa11", "PETROL", 12.4, 50, 32);
-        allFuleUsers.add(bus1);
+        allFuelUsers.add(bus1);
         Truck truck1 = new Truck("Scanie", "tt23", "DIESEL", 21.4, 50, 4000);
-        allFuleUsers.add(truck1);
+        allFuelUsers.add(truck1);
         ElectricityGenerator electricityGenerator = new ElectricityGenerator("Mercedes", 6.2, "DIESEL");
-        allFuleUsers.add(electricityGenerator);
+        allFuelUsers.add(electricityGenerator);
         FuelStorage fuelStorage1 = new FuelStorage(300, "DIESEL");
         FuelStorage fuelStorage2= new FuelStorage(200, "PETROL");
         FuelStorage.addFuel(200, "DIESEL");
         FuelStorage.addFuel(300, "PETROL");
-        System.out.println(Reports.balanceDieselDays(allFuleUsers));
-        System.out.println(Reports.balancePetrolDays(allFuleUsers));
+        System.out.println(Reports.balanceDieselDays(allFuelUsers));
+        System.out.println(Reports.balancePetrolDays(allFuelUsers));
     }
 }
